f836aa85b3cb0662ded8fb87aa6728c42644a259
VladimirTi <tytmes@mail.ru>
2019-04-25 11:06:54 +0300

    Vladimir_Solution

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 'use strict';
+/*
+function someFunc(form) {
+  let name = form.name.value;
+  let surname = form.surname.value;
+  alert(`Information from ${name} ${surname} has been recived by JS`);
+}
+*/
