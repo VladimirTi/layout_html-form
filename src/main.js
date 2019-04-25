473894fb0a385725853ab1d42f710194ef9192c7
VladimirTi <tytmes@mail.ru>
2019-04-25 12:01:26 +0300

    correction

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
 'use strict';
-/*
-function someFunc(form) {
-  let name = form.name.value;
-  let surname = form.surname.value;
-  alert(`Information from ${name} ${surname} has been recived by JS`);
-}
-*/
